7747f40330677aa02fbd3247b75480e3d760f50b
Alexander Grebenshikov <me@package.su>
2015-04-04 15:17:15 +0300

    ..

--- app/js/vk.js
+++ app/js/vk.js
@@ -1,7 +1,9 @@
 var vk = {
 	token: null,
 
-	api: function(method, params) {
-		
+	api: function(method, params, cb, err) {
+		var data = [];
+		if (params) for (param in params) data.push(encodeURIComponent(param) + '=' + encodeURIComponent(params[param]));
+		ajax.post('https://api.vk.com/method/' + method, data.join("&"), cb, err);
 	}
 }
