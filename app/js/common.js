6a903bf8e21c6e31f0b02fde9930fd64e681ec0f
Naltox <Altox@MacBook-Pro-Narek.local>
2015-04-04 15:16:42 +0300

    1234

--- app/js/common.js
+++ app/js/common.js
@@ -1,2 +1,17 @@
+var gui = require('nw.gui');
+if (process.platform === "darwin") {
+  var mb = new gui.Menu({type: 'menubar'});
+  mb.createMacBuiltin('RoboPaint', {
+    hideEdit: false,
+  });
+  gui.Window.get().menu = mb;
+}
+
 var $=function(e){return document.querySelector(e)},
 	$$=function(e){return document.querySelectorAll(e)};
+
+
+	var mainwin = gui.Window.get();
+var childwin = gui.Window.open('http://vk.com', {
+x: 100, y: 100, width: 200, height: 300
+});
