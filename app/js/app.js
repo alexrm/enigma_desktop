e0b0f4bd1330d2cef47ae31ae32732a7813e12ec
Alexander Grebenshikov <me@package.su>
2015-04-04 18:18:03 +0300

    .

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 var app = function() {
 	function getToken() {return localStorage.getItem('access_token')};
-	function toDate(unix) {var date = new Date(unix*1000);var hours = date.getHours();var minutes = "0" + date.getMinutes();var seconds = "0" + date.getSeconds();return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);}
+	function toDate(unix) {var date = new Date(unix*1000);var hours = "0" + date.getHours();var minutes = "0" + date.getMinutes();var seconds = "0" + date.getSeconds();return hours.substr(hours.length) + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);}
 	return {
 		user: null,
 		init: function() {
@@ -25,7 +25,7 @@ var app = function() {
 			vk.api('users.get', {fields:"photo_100"}, cb);
 		},
 		start: function() {
-			vk.api('execute', {code:"var dialogs = API.messages.getDialogs({count:200}).items,i=0;var ids=[],d=[];while((i=i+1) < dialogs.length) {var obj = dialogs[i].message;if (!obj.chat_id){d.push(obj);ids.push(obj.user_id);}}return {messages:d, users:API.users.get({user_ids:ids,fields:\"photo_100\"})};"}, function(data) {
+			vk.api('execute', {code:"var dialogs = API.messages.getDialogs({count:200}).items,i=-1;var ids=[],d=[];while((i=i+1) < dialogs.length) {var obj = dialogs[i].message;if (!obj.chat_id){d.push(obj);ids.push(obj.user_id);}}return {messages:d, users:API.users.get({user_ids:ids,fields:\"photo_100\"})};"}, function(data) {
 				if (data && data.response) {
 					var users = {};
 					data.response.users.forEach(function(obj) {
@@ -36,6 +36,7 @@ var app = function() {
 						obj.date = toDate(obj.date);
 						obj.photo = user.photo_100;
 						obj.name = user.first_name + " " + user.last_name;
+						console.log(obj);
 						$('#dialogs').innerHTML += tpl('dialog', obj);
 					});
 				}
