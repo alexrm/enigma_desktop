a112a3cf86405e04be0e56268b53e0d25cf46ea5
Alexander Grebenshikov <me@package.su>
2015-04-04 16:48:21 +0300

    …

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,12 @@ var app = function() {
 			vk.token = getToken();
 			this.check(function(res) {
 				if (res && res.response) {
-					localStorage.setItem('profile', JSON.stringify(res.response[0]));
+					var profile = res.response[0];
+					localStorage.setItem('profile', JSON.stringify(profile));
 					ajax.get('pages/main.html', function(data){ 
 						document.body.innerHTML = data;
-
+						$('#user_name').innerHTML = profile.firstName + " " + profile.lastName;
+						$('#user_pic').src = profile.photo_100;
 					});
 				} else {
 					vk.auth();
